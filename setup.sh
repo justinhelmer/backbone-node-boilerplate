1b1dfb71c1e40a2f8d24eb1fa1d033ed649cc0bd
Justin Helmer <m469982@a3836945.hsd1.ca.comcast.net>
2013-08-23 19:13:04 -0700

    adding platform-agnostic setup.sh

--- setup.sh
+++ setup.sh
@@ -1,5 +1,24 @@
-npm install -g grunt-cli
-npm install -g bower
-npm install -g requirejs
-npm install -g handlebars
-gem install compass
+SUDO="yes"
+
+case "$OSTYPE" in
+  msys*)    SUDO="no" ;;
+  solaris*) SUDO="yes" ;;
+  darwin*)  SUDO="yes" ;;
+  linux*)   SUDO="yes" ;;
+  bsd*)     SUDO="yes" ;;
+  *)        SUDO="yes" ;;
+esac
+
+if [[ "$SUDO" == "yes" ]]; then
+  sudo npm install -g grunt-cli
+  sudo npm install -g bower
+  sudo npm install -g requirejs
+  sudo npm install -g handlebars
+  sudo gem install compass
+elif [[ "$SUDO" == "no" ]]; then
+  npm install -g grunt-cli
+  npm install -g bower
+  npm install -g requirejs
+  npm install -g handlebars
+  gem install compass
+fi
