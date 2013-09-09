6b33f0a54334fcba71787490f1554e6e51978908
Justin Helmer <jhelmer25@gmail.com>
2013-09-09 10:56:29 -0700

    remove invalid references

--- onestep.sh
+++ onestep.sh
@@ -1,1 +1,1 @@
-git clone git@github.com:emalinc/emal2_web.git && cd emal2_web && chmod +x setup.sh && ./setup.sh && grunt build:dev --browser=true
+git clone git@github.com:justinhelmer/backbone-node-boilerplate.git && cd backbone-node-boilerplate && chmod +x setup.sh && ./setup.sh && grunt build:dev --browser
